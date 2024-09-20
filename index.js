40e9421b3427eb0aa8e548c5240f9a736b793ab0
yassinemontassar <sawthegamer70@gmail.com>
2024-09-20 10:59:10 +0100

    File Metadata Microservice

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+    res.sendFile(process.cwd() + '/views/index.html');
 });
 
 
@@ -17,4 +17,20 @@ app.get('/', function (req, res) {
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
+});
+
+
+// ------- //
+// MY CODE //
+// ------ //
+
+const multer = require('multer');
+const upload = multer();
+
+
+app.post('/api/fileanalyse', 
+  upload.single('upfile'),
+  (req, res) => {
+    const { originalname, mimetype, size } = req.file;
+    res.json({ name: originalname, type: mimetype, size: size });
 });
